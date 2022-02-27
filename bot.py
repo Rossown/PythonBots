b18f56a114ea54f35d162a227b0dd65df97abcda
Nate <rosso1n@cmich.edu>
2022-02-27 11:59:40 -0500

    testing

--- bot.py
+++ bot.py
@@ -12,12 +12,10 @@ from newsapi import NewsApiClient
 
 description = '''Main Discord bot for The Land of the Free!
 
-Hopefully this will do something useful:)
-'''
+Hopefully this will do something useful:)'''
+
 intents = discord.Intents.default()
 intents.members = True
-
-bot = commands.Bot(command_prefix='>', description=description, intents=intents)
 
 
 dotenv_path = join(dirname(__file__), '..', '.env')
@@ -31,27 +29,29 @@ newsapi = NewsApiClient(api_key=NEWSKEY)
 
 members = []
 
+bot = commands.Bot(command_prefix='>', description=description, intents=intents)
 
-async def getNews():
-    # /v2/top-headlines
-    top_headlines = newsapi.get_top_headlines(q='Xbox',
-                                            sources='IGN,the-verge',
-                                            category='business',
-                                            language='en',
-                                            country='us')
 
-    # /v2/everything
-    all_articles = newsapi.get_everything(q='bitcoin',
-                                        sources='bbc-news,the-verge',
-                                        domains='bbc.co.uk,techcrunch.com',
-                                        from_param='2017-12-01',
-                                        to='2017-12-12',
-                                        language='en',
-                                        sort_by='relevancy',
-                                        page=2)
+# async def getNews():
+#     # /v2/top-headlines
+#     top_headlines = newsapi.get_top_headlines(q='Xbox',
+#                                             sources='IGN,the-verge',
+#                                             category='business',
+#                                             language='en',
+#                                             country='us')
 
-    # /v2/sources
-    sources = newsapi.get_sources()
+#     # /v2/everything
+#     all_articles = newsapi.get_everything(q='bitcoin',
+#                                         sources='bbc-news,the-verge',
+#                                         domains='bbc.co.uk,techcrunch.com',
+#                                         from_param='2017-12-01',
+#                                         to='2017-12-12',
+#                                         language='en',
+#                                         sort_by='relevancy',
+#                                         page=2)
+
+#     # /v2/sources
+#     sources = newsapi.get_sources()
 
 async def getmeme():
     resp = requests.get('https://meme-api.herokuapp.com/gimme')
@@ -71,66 +71,66 @@ async def meme(ctx):
 
 @bot.event
 async def on_ready():
-    print(bot.guilds)
-    for guild in bot.guilds:
-        if guild.name == GUILD:
-            break
+    # print(bot.guilds)
+    # for guild in bot.guilds:
+    #     if guild.name == GUILD:
+    #         break
     
-    for guilds in bot.guilds:
-        for member in guild.members:
-            members.append(member)
+    # for guilds in bot.guilds:
+    #     for member in guild.members:
+    #         members.append(member)
     
-    print([member.name for member in members])
+    # print([member.name for member in members])
 
 
     print(
         f'{bot.user} (ID: {bot.user.id}) has connected to Discord!\n'
-        f'{guild.name} (ID: {guild.id})'
+        # f'{guild.name} (ID: {guild.id})'
         )
 
 
-@bot.event
-async def on_member_join(member):
+# @bot.event
+# async def on_member_join(member):
     
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to The Land of the Free!'
-    )
-    print(member.roles)
+#     await member.create_dm()
+#     await member.dm_channel.send(
+#         f'Hi {member.name}, welcome to The Land of the Free!'
+#     )
+#     print(member.roles)
 
-@bot.event
-async def on_reaction_add(reaction, user):
+# @bot.event
+# async def on_reaction_add(reaction, user):
 
-    print(reaction)
-    print(user)
+#     print(reaction)
+#     print(user)
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
+# @bot.event
+# async def on_message(message):
+#     if message.author == bot.user:
+#         return
 
-    elif "bad bot" in str(message.content).lower():
-        print(message.author)
-        if str(message.author)[:-5] == "TitanGusang":
-            response = 'Bitch'
-        else:
-            response = "I am sorry for my actions, " + str(message.author)[:-5]
+#     elif "bad bot" in str(message.content).lower():
+#         print(message.author)
+#         if str(message.author)[:-5] == "TitanGusang":
+#             response = 'Bitch'
+#         else:
+#             response = "I am sorry for my actions, " + str(message.author)[:-5]
 
-        await message.channel.send(response)
+#         await message.channel.send(response)
 
-    elif "good bot" in str(message.content).lower():
-        response = 'Thank you kind human'
-        await message.channel.send(response)
+#     elif "good bot" in str(message.content).lower():
+#         response = 'Thank you kind human'
+#         await message.channel.send(response)
 
-    elif "apologize bot" in str(message.content).lower():
-        response = "I am sorry"
-        await message.channel.send(response)
+#     elif "apologize bot" in str(message.content).lower():
+#         response = "I am sorry"
+#         await message.channel.send(response)
 
-    elif 'happy birthday' in message.content.lower():
-        await message.channel.send('Happy Birthday! :balloon: :birthday:')
+#     elif 'happy birthday' in message.content.lower():
+#         await message.channel.send('Happy Birthday! :balloon: :birthday:')
 
-    elif str(message.content).lower() == '>suck a dick':
-        await message.channel.send('https://tenor.com/view/yes-hamster-carrot-bj-blow-job-gif-15498598')
+#     elif str(message.content).lower() == '>suck a dick':
+#         await message.channel.send('https://tenor.com/view/yes-hamster-carrot-bj-blow-job-gif-15498598')
  
 
 bot.run(TOKEN)
